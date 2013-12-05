6f93b9b4d1a81b39e2c94af46fff82a0f5e11db9
Serg Bozhko <stwnsn@gmail.com>
2013-12-05 22:57:05 +0400

    Last version

--- quadratic-equation.js
+++ quadratic-equation.js
@@ -1,34 +1,38 @@
 /**
- * Created by serg on 03.12.13.
+ *@param {number} a;
+ *@param {number} b;
+ *@param {number} c;
  */
-var a = 2;
-var b = 6;
-var c = 4;
-
-if (a != null && b != null && c != null)
-{
-  var d = Math.pow(b, 2) - (4 * a * c);
-  if (d >= 0)
+function quadraticEquation(a, b, c) {
+  var x1;
+  var x2;
+  if (typeof (a) === 'number' &&
+      typeof (b) === 'number' &&
+      typeof (c) === 'number')
   {
-    if (d == 0)
+    var d = Math.pow(b, 2) - (4 * a * c);
+    if (d >= 0)
     {
-      var x1 = (-b + Math.sqrt(d)) / (2 * a);
-      console.log('x1 = x2 = ' + x1);
+      if (d == 0)
+      {
+        x1 = x2 = (-b + Math.sqrt(d)) / (2 * a);
+      }
+      else
+      {
+        x1 = (-b + Math.sqrt(d)) / (2 * a);
+        x2 = (-b - Math.sqrt(d)) / (2 * a);
+      }
+      console.log('x1= ' + x1 + '; x2= ' + x2);
     }
     else
     {
-      var x1 = (-b + Math.sqrt(d)) / (2 * a);
-      var x2 = (-b - Math.sqrt(d)) / (2 * a);
-      console.log('x1= ' + x1 + '; x2= ' + x2);
+      console.error('Нет корней !');
     }
   }
   else
   {
-    console.log('Нет корней !');
+    console.error('Введите переменные !');
   }
 }
-else
-{
-  console.log('Введите переменные !');
-}
+quadraticEquation(2 , 6 , 4);
 
