523ce5660d8a854cfc6b993813bd1130ac7cf35f
Nikolay Kapustin <nik.strange@yandex.ru>
2013-12-10 19:47:41 +0400

    Создал функцию сервера для функции расчета квадратного уравнения

--- quadratic-equation.js
+++ quadratic-equation.js
@@ -3,27 +3,40 @@
  * @param {number} b
  * @param {number} c
  */
-function quadraticEquation(a, b, c) {
+var express = require('express');
+var app = express();
+app.get('/:a/:b/:c', function(req, res){
+  res.send(quadro(parseInt(req.params.a), parseInt(req.params.b),parseInt(req.params.c)));
+});
+
+app.listen(3100);
+
+
+var quadro = function quadraticEquation(a, b, c) {
   var x1;
   var x2;
+  var res = {
+    x1:0,
+    x2:0
+  }
   if (typeof (a) === 'number' &&
       typeof (b) === 'number' &&
       typeof (c) === 'number') {
     var d = Math.pow(b, 2) - (4 * a * c);
     if (d >= 0) {
       if (d == 0) {
-        x1 = x2 = (-b + Math.sqrt(d)) / (2 * a);
+        res.x1 = res.x2 = (-b + Math.sqrt(d)) / (2 * a);
       } else {
-        x1 = (-b + Math.sqrt(d)) / (2 * a);
-        x2 = (-b - Math.sqrt(d)) / (2 * a);
+        res.x1 = (-b + Math.sqrt(d)) / (2 * a);
+        res.x2 = (-b - Math.sqrt(d)) / (2 * a);
       }
-      console.log('x1= ' + x1 + '; x2= ' + x2);
+      return res;
     } else {
-      console.error('Нет корней !');
+      return ('Нет корней !');
     }
   } else {
-    console.error('Введите переменные !');
+    return ('Введите переменные !');
   }
 }
-quadraticEquation(2 , 6 , 4);
+
 
